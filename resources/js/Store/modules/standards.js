253bdccd3d88f4ce5c05d571e70b157cf128bada
Kevin Marete <kevinmmarete@gmail.com>
2023-03-19 11:58:04 -0400

    Added removeItemFromStandards to Standards Store

--- resources/js/Store/modules/standards.js
+++ resources/js/Store/modules/standards.js
@@ -13,13 +13,19 @@ const getters = {
 const actions = {
   addItemToStandards ({ state, commit }, standard) {
         commit('addItemToStandards', { standard })
-    }
+  },
+  removeItemFromStandards({ state, commit }, standard) {
+      commit('removeItemFromStandards', { standard })
+  }
 }
 
 // mutations
 const mutations = {
     addItemToStandards (state, { standard }) {
         state.items.push(standard)
+    },
+    removeItemFromStandards(state, { standard }){
+        state.items = state.items.filter(item => item["key"] !== standard["key"])
     }
 }
 
